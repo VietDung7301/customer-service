1b9fcb571a74a27d93700cd900c8e5ee692b1911
vietdung7301 <vietdung.7301@gmail.com>
2023-02-23 09:27:14 +0700

    Sửa lỗi CORS khi deploy

--- client/src/app/services/orderForm/order-form.service.ts
+++ client/src/app/services/orderForm/order-form.service.ts
@@ -1,31 +1,31 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderFormService {
-  private REST_API = 'http://103.179.173.95:81/api/listOrderByUser/21';
+  private REST_API = environment.orderAPI;
   private REST_API_POST_DATA ='http://localhost:8000/user/complain'
   private httpOption ={
     headers :new HttpHeaders({
       'contentType' : 'application/json'
     })
   }
-  constructor(
-    private https : HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   public getOrder() : Observable<any> {
      const url = `${this.REST_API}`;
-     return this.https.get<any>(url,this.httpOption);
+     return this.http.get<any>(url);
 
   }
 
   public postData(data: any) : Observable<any> {
 
     const url = `${this.REST_API_POST_DATA}`;
-    return this.https.post<any>(url,data,this.httpOption);
+    return this.http.post<any>(url,data);
 
  }
 
